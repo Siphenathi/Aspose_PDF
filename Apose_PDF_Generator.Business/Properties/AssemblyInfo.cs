d3a9541fe6af2026c2e8098e09dadbd14a3e1a28
Siphenathi Pantshwa <SiphenathiP@SAHOMELOANS.COM>
2018-06-21 13:42:37 +0200

    Completed Exercise

--- Apose_PDF_Generator.Business/Properties/AssemblyInfo.cs
+++ Apose_PDF_Generator.Business/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Apose_PDF_Generator.Business")]
+[assembly: AssemblyTitle("Apose_PDF_Generator.Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Apose_PDF_Generator.Business")]
+[assembly: AssemblyProduct("Apose_PDF_Generator.Service")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Apose_PDF_Generator/Program.cs
+++ Apose_PDF_Generator/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Apose_PDF_Generator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
